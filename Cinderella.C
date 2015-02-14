b7da37ccf33e067a6f5e4746998892fca65bdd06
Gabriel Teixeira <gabrielteixeira314@gmail.com>
2015-02-14 13:53:54 -0500

    Update Cinderella.C

--- Cinderella.C
+++ Cinderella.C
@@ -1,4 +1,5 @@
 {
+	// Create a tree and fill it with the data from the Cinderella DB
 	TTree *tree = new TTree("tree", "tree");
 	tree->ReadFile("snapshotCinderellaDb.txt", "name/C:nused/I:ndownloads:incache:entrydate:size/F");
 	Char_t name;
@@ -11,8 +12,10 @@
 	tree->SetBranchAddress("entrydate",&entrydate);
 	tree->SetBranchAddress("size",&size);
 
+	// Declare the variable nentries
 	Long64_t nentries = tree->GetEntries();
 
+	// Create six histograms
 	TH1F *h1 = new TH1F("h1","",50,0,209);
 	TH1F *h2 = new TH1F("h2","",50,0,10000000000000);
 	TH1F *h3 = new TH1F("h3","",100,0,0.063);
@@ -20,6 +23,7 @@
 	TH1F *h5 = new TH1F("h5","",100,0,0.063);
 	TH1F *h6 = new TH1F("h6","",3,0.5,3.5);
 
+	// Format histogram h1 and fill it with the variable nused to create a histogram of dataset popularity
 	h1->GetXaxis()->SetTitle("Number of Accesses");
 	h1->GetYaxis()->SetTitle("Frequency");
 	h1->SetStats(0);
@@ -28,49 +32,63 @@
 	gPad->SetLogy();
 	c1->SaveAs("Popularity.png");
 	
+	// Format histogram h2 and fill it with the variable size to create a histogram of dataset sizes
 	h2->GetXaxis()->SetTitle("Size of Dataset [GB]");
 	h2->GetYaxis()->SetTitle("Frequency");
 	h2->SetStats(0);
 	tree->Draw("size>>h2");
 	c1->SaveAs("Size.png");
 
+	// Format histogram h3
+	// Declare a variable sizeSum and loop through the entries to add all the sizes of the datasets
+	// Create a string that calculates the occupancy of each dataset and fill the histogram with those values
 	h3->GetXaxis()->SetTitle("Ocuppancy per Dataset [Percentage]");
 	h3->GetYaxis()->SetTitle("Frequency");
 	h3->SetStats(0);
 	float sizeSum = 0;
    	for (Long64_t i=0;i<nentries;i++) {
-     	tree->GetEntry(i);
-     	sizeSum += size;
-    }
-  TString occupancy = Form("size/%f>>h3",sizeSum);
+     		tree->GetEntry(i);
+     		sizeSum += size;
+	 }
+  	TString occupancy = Form("size/%f>>h3",sizeSum);
 	tree->Draw(occupancy);
 	c1->SaveAs("Occupancy.png");
 
+	// Format histogram h4
+	// Loop through the entries and fill h4 with the size of any dataset whose value for incache is 1
 	h4->GetXaxis()->SetTitle("Size of Datasets in Cache [GB]");
 	h4->GetYaxis()->SetTitle("Frequency");
 	h4->SetStats(0);
 	for (Long64_t i=0;i<nentries;i++) {
-		tree->GetEntry(i);
-		if(incache==1)
-			h4->Fill(size);
+	  tree->GetEntry(i);
+	  if(incache==1)
+	  	h4->Fill(size);
 	}
 	h4->Draw();
 	c1->SaveAs("inCacheSize.png");
 
+	// Format histogram h5
+	// Declare the variable inCacheSum
+	// Loop through the entries, adding the sizes of those datasets whose incache value is 1
+	// Create a string that calculates the occupancy of each datasets whose incache value is 1
+	// and fills h5 with said values
 	h5->GetXaxis()->SetTitle("Occupancy per Dataset in Cache [Percentage]");
-  h5->GetYaxis()->SetTitle("Frequency");
-  h5->SetStats(0);
-  float inCacheSum = 0;
-  for (Long64_t i=0;i<nentries;i++) {
+	h5->GetYaxis()->SetTitle("Frequency");
+ 	h5->SetStats(0);
+ 	float inCacheSum = 0;
+  	for (Long64_t i=0;i<nentries;i++) {
 	  tree->GetEntry(i);
 	  if(incache==1)
-		  inCacheSum += size;
-		  TString inCacheOccupancy = Form("size/%f>>h5",inCacheSum);
-		  tree->Draw(inCacheOccupancy);
+		inCacheSum += size;
+	  	TString inCacheOccupancy = Form("size/%f>>h5",inCacheSum);
+	  	tree->Draw(inCacheOccupancy);
 	}
 	c1->SaveAs("inCacheOccupancy.png");
 
-  h6->GetXaxis()->SetTitle("Number of Downloads");
+	// Format h6 and fill it with ndownloads
+	// Create a pie from h6
+	// Create a legend for the pie chart
+	h6->GetXaxis()->SetTitle("Number of Downloads");
 	h6->GetYaxis()->SetTitle("Frequency");
 	h6->SetStats(0);
 	tree->Draw("ndownloads>>h6");
