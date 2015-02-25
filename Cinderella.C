02086f594b8bef70838d46b8dff0b320aae522f3
Gabriel Teixeira <gabrielteixeira314@gmail.com>
2015-02-25 16:12:47 -0500

    Update Cinderella.C

--- Cinderella.C
+++ Cinderella.C
@@ -1,5 +1,4 @@
 {
-	// Create a tree and fill it with the data from the Cinderella DB
 	TTree *tree = new TTree("tree", "tree");
 	tree->ReadFile("snapshotCinderellaDb.txt", "name/C:nused/I:ndownloads:incache:entrydate:size/F");
 	Char_t name;
@@ -12,101 +11,135 @@
 	tree->SetBranchAddress("entrydate",&entrydate);
 	tree->SetBranchAddress("size",&size);
 
-	// Declare the variable nentries
 	Long64_t nentries = tree->GetEntries();
 
-	// Create six histograms
+	TDatime date;
+	TString dateTime = date.AsSQLString();
+	TString text;
+	text = TString("Last Update: ") + dateTime;
+	TText *update = new TText(0.025,0.025,text.Data());
+	update->SetTextSize(0.04);
+	update->SetTextColor(kBlue);
+
 	TH1F *h1 = new TH1F("h1","",50,0,209);
-	TH1F *h2 = new TH1F("h2","",50,0,10000000000000);
+	TH1F *h2 = new TH1F("h2","",50,0,10000);
 	TH1F *h3 = new TH1F("h3","",100,0,0.063);
-	TH1F *h4 = new TH1F("h4","",50,0,10000000000000);
+	TH1F *h4 = new TH1F("h4","",50,0,10000);
 	TH1F *h5 = new TH1F("h5","",100,0,0.063);
 	TH1F *h6 = new TH1F("h6","",3,0.5,3.5);
 
-	// Format histogram h1 and fill it with the variable nused to create a histogram of dataset popularity
-	h1->GetXaxis()->SetTitle("Number of Accesses");
-	h1->GetYaxis()->SetTitle("Frequency");
-	h1->SetStats(0);
-	tree->Draw("nused>>h1");
-	gPad->SetLogx();
-	gPad->SetLogy();
+	TPie *pie1 = new TPie("","",3);
+	pie1->SetRadius(0.3);
+	float slice0 = 0;
+	float slice1 = 0;
+	float slice2 = 0;
+	for (Long64_t i=0;i<nentries;i++) {
+     	  tree->GetEntry(i);
+     	  if(nused<10){
+     	    slice0 += 1;
+     	  }
+     	  if(nused>=10,nused<100){
+     	    slice1 += 1;
+     	  }
+     	  if(nused>=100){
+     	    slice2 += 1;
+     	  }
+        }
+        pie1->GetSlice(0)->SetValue(slice0);
+    	pie1->GetSlice(1)->SetValue(slice1);
+    	pie1->GetSlice(2)->SetValue(slice2);
+	pie1->SetEntryFillColor(0,4);
+	pie1->SetEntryFillColor(1,3);
+	pie1->SetEntryFillColor(2,2);
+	pie1->SetEntryLabel(0,"Less than 10 accesses");
+	pie1->SetEntryLabel(1,"10-100 accesses");
+	pie1->SetEntryLabel(2,"100+ accesses");
+	pie1->SetLabelFormat("%perc");
+	pie1->Draw("nol");
+	TLegend *leg1 = pie1->MakeLegend();
+ 	leg1->SetY1(.77); leg1->SetY2(.95);
+ 	leg1->SetHeader("Popularity");
+	update->DrawTextNDC(0.025,0.025,text.Data());
 	c1->SaveAs("Popularity.png");
+	c1->Clear();
 	
-	// Format histogram h2 and fill it with the variable size to create a histogram of dataset sizes
 	h2->GetXaxis()->SetTitle("Size of Dataset [GB]");
 	h2->GetYaxis()->SetTitle("Frequency");
 	h2->SetStats(0);
-	tree->Draw("size>>h2");
+	float gigabyteScaling = 1000000000;
+	TString sizeOfDatasets = Form("size/%f>>h2",gigabyteScaling);
+	tree->Draw(sizeOfDatasets);
+	gPad->SetLogx();
+	gPad->SetLogy();
+	update->DrawTextNDC(0.025,0.025,text.Data());
 	c1->SaveAs("Size.png");
+	c1->Clear();
 
-	// Format histogram h3
-	// Declare a variable sizeSum and loop through the entries to add all the sizes of the datasets
-	// Create a string that calculates the occupancy of each dataset and fill the histogram with those values
-	h3->GetXaxis()->SetTitle("Occupancy per Dataset");
+	h3->GetXaxis()->SetTitle("Ocuppancy per Dataset [Percentage]");
 	h3->GetYaxis()->SetTitle("Frequency");
 	h3->SetStats(0);
 	float sizeSum = 0;
    	for (Long64_t i=0;i<nentries;i++) {
-     		tree->GetEntry(i);
-     		sizeSum += size;
-	 }
-  	TString occupancy = Form("size/%f>>h3",sizeSum);
+     	  tree->GetEntry(i);
+     	  sizeSum += size;
+        }
+        TString occupancy = Form("size/%f>>h3",sizeSum);
 	tree->Draw(occupancy);
+	update->DrawTextNDC(0.025,0.025,text.Data());
 	c1->SaveAs("Occupancy.png");
+	c1->Clear();
 
-	// Format histogram h4
-	// Loop through the entries and fill h4 with the size of any dataset whose value for incache is 1
 	h4->GetXaxis()->SetTitle("Size of Datasets in Cache [GB]");
 	h4->GetYaxis()->SetTitle("Frequency");
 	h4->SetStats(0);
 	for (Long64_t i=0;i<nentries;i++) {
 	  tree->GetEntry(i);
-	  if(incache==1)
-	  	h4->Fill(size);
+	  if(incache==1){
+	    h4->Fill(.000000001*size);
+	  }
 	}
 	h4->Draw();
+	update->DrawTextNDC(0.025,0.025,text.Data());
 	c1->SaveAs("inCacheSize.png");
+	c1->Clear();
 
-	// Format histogram h5
-	// Declare the variable inCacheSum
-	// Loop through the entries, adding the sizes of those datasets whose incache value is 1
-	// Create a string that calculates the occupancy of each datasets whose incache value is 1
-	// and fills h5 with said values
-	h5->GetXaxis()->SetTitle("Occupancy per Dataset in Cache");
-	h5->GetYaxis()->SetTitle("Frequency");
- 	h5->SetStats(0);
- 	float inCacheSum = 0;
-  	for (Long64_t i=0;i<nentries;i++) {
+	h5->GetXaxis()->SetTitle("Occupancy per Dataset in Cache [Percentage]");
+   	h5->GetYaxis()->SetTitle("Frequency");
+   	h5->SetStats(0);
+   	float inCacheSum = 0;
+   	for (Long64_t i=0;i<nentries;i++) {
 	  tree->GetEntry(i);
-	  if(incache==1)
-		inCacheSum += size;
-	  	TString inCacheOccupancy = Form("size/%f>>h5",inCacheSum);
-	  	tree->Draw(inCacheOccupancy);
+	  if(incache==1){
+	    inCacheSum += size;
+	    TString inCacheOccupancy = Form("size/%f>>h5",inCacheSum);
+	    tree->Draw(inCacheOccupancy);
+	  }
 	}
+	update->DrawTextNDC(0.025,0.025,text.Data());
 	c1->SaveAs("inCacheOccupancy.png");
+	c1->Clear();
 
-	// Format h6 and fill it with ndownloads
-	// Create a pie from h6
-	// Create a legend for the pie chart
-	h6->GetXaxis()->SetTitle("Number of Downloads");
+   	h6->GetXaxis()->SetTitle("Number of Downloads");
 	h6->GetYaxis()->SetTitle("Frequency");
 	h6->SetStats(0);
 	tree->Draw("ndownloads>>h6");
 	gPad->SetLogx(0);
 	gPad->SetLogy(0);
-	TPie *pie = new TPie(h6);
-	pie->SetRadius(.3);
-	pie->SetEntryFillColor(0,4);
-	pie->SetEntryFillColor(1,3);
-	pie->SetEntryFillColor(2,2);
-	pie->SetEntryLabel(0,"1 Download");
-	pie->SetEntryLabel(1,"2 Downloads");
-	pie->SetEntryLabel(2,"3 Downloads");
-	pie->SetLabelFormat("%perc");
-	pie->Draw("nol");
-	TLegend *leg = pie->MakeLegend();
- 	leg->SetY1(.77); leg->SetY2(.95);
- 	leg->SetHeader("Number of Downloads");
+	TPie *pie2 = new TPie(h6);
+	pie2->SetRadius(.3);
+	pie2->SetEntryFillColor(0,4);
+	pie2->SetEntryFillColor(1,3);
+	pie2->SetEntryFillColor(2,2);
+	pie2->SetEntryLabel(0,"1 Download");
+	pie2->SetEntryLabel(1,"2 Downloads");
+	pie2->SetEntryLabel(2,"3 Downloads");
+	pie2->SetLabelFormat("%perc");
+	pie2->Draw("nol");
+	TLegend *leg2 = pie2->MakeLegend();
+ 	leg2->SetY1(.77); leg2->SetY2(.95);
+ 	leg2->SetHeader("Number of Downloads");
+	update->DrawTextNDC(0.025,0.025,text.Data());
 	c1->SaveAs("nDownloads.png");
+	c1->Clear();	
 
 }
